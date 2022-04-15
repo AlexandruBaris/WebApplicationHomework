99525e4425364e5778911b03f54869e887789a99
ABarisenschi <alexandru.barisenschi@gmail.com>
2022-04-15 12:56:36 +0300

    First commit

---
+++ src/main/java/com/homework/homeworkWebApp/HomeworkWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.homework.homeworkWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homework/homeworkWebApp/HomeworkWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.homeworkWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
